36e4904dffe216f25361450b8372c49766a10583
jahid28 <jahidrhps123@gmail.com>
2024-03-30 23:33:51 +0530

    first commit

---
+++ pg.js
@@ -0,0 +1,59 @@
+const { Client } =require('pg');
+
+const client = new Client({
+  user: 'postgres',
+  host: 'postgre-db-instance-1.cfc6wmay0j3n.us-east-1.rds.amazonaws.com',
+  database: 'postgredb1',
+  password: 'Jahidkhan786',
+  port: 5432, // Default PostgreSQL port
+});
+// const client = new Client({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'db1',
+//   password: 'Jahidkhan@786',
+//   port: 5432, // Default PostgreSQL port
+// });
+
+// async function connectToDatabase() {
+//   try {
+//     await client.connect();
+//     console.log('Connected to PostgreSQL');
+//   } catch (err) {
+//     console.error('Connection error', err);
+//   }
+// }
+
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('Connection error', err));
+
+
+// async function createTable() {
+//   try {
+//     const createTableQuery = `
+//       CREATE TABLE IF NOT EXISTS users (
+//         id SERIAL PRIMARY KEY,
+//         username VARCHAR(255) NOT NULL,
+//         email VARCHAR(255) NOT NULL UNIQUE
+//       )
+//     `;
+//     await client.query(createTableQuery);
+//     console.log('Table created successfully');
+//   } catch (err) {
+//     console.error('Error creating table', err);
+//   }
+// }
+// async function insert(name,email) {
+//   try {
+//     const query = `
+//       insert into table1(name,email) values($1,$2)`;
+//       const values=[name,email]
+//     await client.query(query,values);
+//     console.log('Table created successfully');
+//   } catch (err) {
+//     console.error('Error creating table', err);
+//   }
+// }
+
+module.exports= {client};
---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const { client } = require("./pg.js");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+// app.set("views", tempelatePath)
+
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const isthere = await client.query(
+      `select count(*) from users where email='${email}'`
+    );
+
+    if (parseInt(isthere.rows[0].count) > 0) {
+      res.send("Email already registered")
+    } else {
+      const query = `
+      insert into users(name,email,password) values($1,$2,$3)`;
+      const values = [name, email, password];
+      await client.query(query, values);
+      res.render("home",{name});
+    }
+  } catch (err) {
+    res.send(err)
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const isthere = await client.query(
+      `select count(*) from users where email='${email}'`
+    );
+    
+    if (parseInt(isthere.rows[0].count) == 0) {
+      res.send('Email not registered');
+    }
+    else{
+      const passFromTable = await client.query(
+        `select * from users where email='${email}'`
+      );
+
+      if(passFromTable.rows[0].password==password){
+        res.render("home",{name:passFromTable.rows[0].name})
+        // res.json({ message: 'correct' ,name:passFromTable.rows[0].name});
+
+      }
+      else{
+        res.send('Incorrect password');
+
+      }
+  
+
+    }
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
